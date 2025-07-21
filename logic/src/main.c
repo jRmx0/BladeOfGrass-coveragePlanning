408e2262deaef80858c59544f3d8487cf26ca181
JFour404 <justas00001@gmail.com>
2025-07-21 13:09:26 +0300

    cJSON Hello world

---
+++ logic/src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "cJSON/cJSON.h"
+
+int main()
+{
+    cJSON *json = cJSON_Parse("{\"msg\":\"Hello, world!\"}");
+    const cJSON *msg = cJSON_GetObjectItemCaseSensitive(json, "msg");
+    if (cJSON_IsString(msg) && (msg->valuestring != NULL)) {
+        printf("msg: %s\n", msg->valuestring);
+    }
+    cJSON_Delete(json);
+    return 0;
+}
